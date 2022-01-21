b858aa04df235abdef398b05176dbd146cb1c947
5FB5 <vecktor911@gmail.com>
2022-01-21 15:55:20 +0300

    Transfered files from personal repo

---
+++ code/notifications/NotificationsHud.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace Notifications
+{
+	[Library]
+	public partial class NotificationsHud : HudEntity<RootPanel>
+	{
+		public class NotificationBase : Panel
+		{
+			public Label Title;
+			public Label Message;
+			public Label NotificationShape;
+
+			public NotificationBase()
+            {
+				NotificationShape = Add.Label(" ", "shape");
+            }
+		}
+
+		public NotificationsHud()
+		{
+			if (!IsClient)
+				return;
+
+			RootPanel.StyleSheet.Load("/notifications/styles/NotificationsStyle.scss");
+
+			RootPanel.AddChild<Generic>();
+			//RootPanel.AddChild<Hint>();
+			//RootPanel.AddChild<Error>();
+		}
+	}
+}
