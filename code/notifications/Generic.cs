c63a4bb5b678e0be5d163a08e507470ea7c3c1ae
5FB5 <vecktor911@gmail.com>
2022-01-25 00:53:35 +0300

    Cleaned up generic and hint's notification code

--- code/notifications/Generic.cs
+++ code/notifications/Generic.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Sandbox;
-using Sandbox.UI;
-using Sandbox.UI.Construct;
 
 namespace Notifications
 {
