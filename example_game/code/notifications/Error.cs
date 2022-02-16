d8ac57a603e240c36255adae8a7c5d961322ca66
5FB5 <vecktor911@gmail.com>
2022-02-16 18:53:39 +0300

    Updated repo structure

---
+++ example_game/code/notifications/Error.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifications
+{
+	public class Error : NotificationBase
+	{
+		public Error()
+		{
+			Title.Delete();
+			Message.Text = "Something is creating error";
+		}
+	}
+}
---
+++ example_game/code/notifications/Hint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifications
+{
+	public class Hint : NotificationBase
+	{
+		public Hint()
+		{
+			Title.Text = "HINT:";
+			Message.Text = "This is a hint message that must notify you about something!";
+		}
+	}
+}
