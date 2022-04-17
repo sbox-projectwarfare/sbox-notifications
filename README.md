db8b6b82a8331ac7796aba39bfb1a5cf9b75b7cf
5FB5 <vecktor911@gmail.com>
2022-04-18 00:51:20 +0300

    Updated repo's structure
    - Now main code splitted to backend and frontend folders
    - Updated README;

--- README.md
+++ README.md
@@ -7,12 +7,12 @@
 Clone this repository to folder you need via `git clone` command or add as submodule via [`git submodule`](https://git-scm.com/book/en/v2/Git-Tools-Submodules) command.
 
 ## <b>How to use it?</b>
-This repository has a main part with library's source code and an example project, that you can use to understand how this library can be used.
+This repository has a main part with library's source code and an example project, that you can use to understand how this library can be used. Keep in mind, in example project backend part stores in `code/notifications` and frontend in `ui/notifications`
 
 To activate library you must:
 * Initialize `NotificationStack` in game script's constructor to store data;
 ![Manager init](readme_img/manager_init.png)
-* Call `NotificationStack.Push` method from it (check "Notifications Library API") with data type you need;
+* Call `NotificationStack.Push` method from it in player's code (check "Notifications Library API") with data type you need;
 ![Calling method](readme_img/manager_shownotification.png)
 * Have fun.
 
@@ -22,7 +22,7 @@ If you want to make your own notification type with custom style, you need to do
 
 ![Custom notification example](readme_img/custom_notification.png)
 
-* Open `styles/NotificationsStyle.scss` file and set style for your custom notification type. For example, since we write "yellow" in class, we should .
+* Open `styles/NotificationsStyle.scss` file and set style for your custom notification type. For example, since we write "yellow" in class, we should. If you have any problems, check styles code above.
 
 ![Style example](readme_img/style_example.png)
 * Now you can call `NotificationStack.Push` method with your new type.
---
+++ example_game/code/notifications/NotificationStack.cs
@@ -0,0 +1,94 @@
+/*
+ * Notification stack script code is under MIT License
+ *
+ * Copyright (c) 2022 s&box MilitaryRP
+ * 
+ * Author: Val Zubko (5FB5)
+ * Contributors: Alf21
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Sandbox;
+
+namespace Warfare.Notifications
+{
+    /// <summary>
+    /// Main class for working with notifications
+    /// </summary>
+    public partial class NotificationStack
+    {
+        public static NotificationStack Instance { get; set; }
+
+        private Stack<NotificationData> Stack { get; set; } = new();
+
+        public int Count
+        {
+            get => Stack.Count;
+        }
+
+        public IEnumerable<NotificationData> All
+        {
+            get => Stack.AsEnumerable();
+        }
+
+        public NotificationStack()
+        {
+            Instance = this;
+        }
+
+        [ClientRpc]
+        public static void AddNotification(string libraryName, byte[] data)
+        {
+            Type notificationDataType = Library.Get<NotificationData>(libraryName);
+
+            if (notificationDataType == null)
+            {
+                return;
+            }
+
+            NotificationData notificationData = Library.Create<NotificationData>(notificationDataType);
+
+            notificationData.Read(data);
+
+            Instance?.Stack.Push(notificationData);
+        }
+
+        public static void Push<T>(To to, T notificationData) where T : NotificationData
+        {
+            if (Host.IsServer)
+            {
+                AddNotification(to, notificationData.Name, notificationData.Write());
+            }
+            else
+            {
+                Instance?.Stack.Push(notificationData);
+            }
+        }
+
+        public static void Push<T>(T notificationData) where T : NotificationData => Push(To.Everyone, notificationData);
+
+        public NotificationData Pop() => Stack.Pop();
+    }
+}
